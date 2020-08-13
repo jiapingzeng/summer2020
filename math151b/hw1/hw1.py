5d854911f3a83023e385ee4b5b679f23bd5676fb
Jiaping Zeng <jzeng@outlook.com>
2020-08-13 16:44:35 -0700

    finished 151b hw1

--- math151b/hw1/hw1.py
+++ math151b/hw1/hw1.py
@@ -1,19 +1,45 @@
+from math import pow, exp
+
+
 def main():
-    q2b()
+    q3c()
 
 
 def q2b():
     h_vals = [0.5, 0.2, 0.1, 0.01]  # step sizes
-    t_0, y_0, t_f = 1.0, 2.0, 2.0  # initial/final values
+    t_0, t_f, y_0 = 1, 2, 2  # initial/final values
 
     for h in h_vals:
-        print(f"h={h}")
         t, y, steps = t_0, y_0, int((t_f-t_0)/h)
         for _ in range(steps):
             y_prev = y
             y = y_prev + h*(1+t)/(1+y_prev)
             t += h
-        print(f"y={y}\n")
+        print(f"h={h}, y={y}")
+
+
+def q3c():
+    h_vals = [0.5, 0.1, 0.01]  # step sizes
+    t_0, t_f, y_0 = 0, 1, 1  # initial/final values
+
+    print("(Euler's method)")
+    for h in h_vals:
+        t, y, steps = t_0, y_0, int((t_f-t_0)/h)
+        for _ in range(steps):
+            y_prev = y
+            y = y_prev + h*pow(y_prev, 2)*exp(-t)
+            t += h
+        print(f"h={h}, y={y}, error={exp(1)-y}")
+
+    print("(Taylor method)")
+    for h in h_vals:
+        t, y, steps = t_0, y_0, int((t_f-t_0)/h)
+        for _ in range(steps):
+            y_prev = y
+            y = y_prev + h*pow(y_prev, 2)*exp(-t) + pow(h, 2) * \
+                (-1*pow(y_prev, 2)*exp(-t)+2*pow(y_prev, 3)*exp(-2*t))/2
+            t += h
+        print(f"h={h}, y={y}, error={exp(1)-y}")
 
 
 if __name__ == "__main__":
