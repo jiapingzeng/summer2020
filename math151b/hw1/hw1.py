4fe819ca3181f1da8bdc4eb08a61e2f3ef0ee4f2
Jiaping Zeng <jzeng@outlook.com>
2020-08-12 21:32:26 -0700

    updated 151a hw1

--- math151b/hw1/hw1.py
+++ math151b/hw1/hw1.py
@@ -1,15 +1,20 @@
 def main():
+    q2b()
+
+
+def q2b():
     h_vals = [0.5, 0.2, 0.1, 0.01]  # step sizes
-    t_0, y_0, t_f = 1, 2, 2  # initial/final values
+    t_0, y_0, t_f = 1.0, 2.0, 2.0  # initial/final values
 
     for h in h_vals:
         print(f"h={h}")
-        t, y = t_0, y_0
-        while t < t_f:
+        t, y, steps = t_0, y_0, int((t_f-t_0)/h)
+        for _ in range(steps):
             y_prev = y
             y = y_prev + h*(1+t)/(1+y_prev)
             t += h
         print(f"y={y}\n")
+
 
 if __name__ == "__main__":
     main()
